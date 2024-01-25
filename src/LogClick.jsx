773d47121f9b73abd23074018540882cbe3aabb1
Hanna922 <hanna.urssu@gmail.com>
2024-01-25 19:21:31 +0900

    chore: setting config file & initial setting

--- src/LogClick.jsx
+++ src/LogClick.jsx
@@ -1,12 +1,9 @@
 import { useYLSLogger } from '.';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+
 export const LogClick = ({ children, params }) => {
   const logger = useYLSLogger();
-  const router = useLocation();
-  const { path } = params;
 
-  return <div></div>;
+  return <></>;
 };
-
-/* 서브루틴으로 뺀 타임스탬프 계산 */
--- src/LogScreen.jsx
+++ src/LogScreen.jsx
@@ -1,6 +1,7 @@
 import { useYLSLogger } from '.';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+
 export const LogScreen = ({ children, params }) => {
   const logger = useYLSLogger();
   const router = useLocation();
@@ -11,7 +12,6 @@ export const LogScreen = ({ children, params }) => {
       logger.screen(path);
     }
   }, []);
+
   return <>{children}</>;
 };
-
-/* 서브루틴으로 뺀 타임스탬프 계산 */
