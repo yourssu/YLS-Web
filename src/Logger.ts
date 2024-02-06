b865f85c53b38d79901c1e18421f23790366d17a
JjungminLee <julie0964@naver.com>
2024-02-06 21:39:10 +0900

    fix: 테스트용 수정

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,4 +1,4 @@
-import { LogPayloadParams, LogType, LoggerType } from './types/LogType';
+import { LogPayloadParams, LoggerType } from './types/LogType';
 
 const createUserId = () => {
   // Todo: create random id
@@ -11,21 +11,9 @@ const createTimestamp = () => {
   return now.toISOString();
 };
 
-const setLocalStorage = (logger: LogType) => {
-  if (window.localStorage.getItem('yls-web') == undefined) {
-    const list: any[] = [];
-    list.push(logger);
-    localStorage.setItem('yls-web', JSON.stringify(list));
-  } else {
-    const remainList: any[] = JSON.parse(localStorage.getItem('yls-web') as string) || [];
-    const updateList = [...remainList, logger];
-    localStorage.setItem('yls-web', JSON.stringify(updateList));
-  }
-};
-
 export const useYLSLogger = () => {
-  const screen = ({ serviceName, name }: LogPayloadParams) => {
-    //사용자에서 path,name,message를 넣어줌
+  const screen = ({ serviceName, path }: LogPayloadParams) => {
+    //사용처에서 userId,name,message를 넣어줌
     const loggerType: LoggerType = {
       path: '/',
       serviceName: 'home',
@@ -34,14 +22,15 @@ export const useYLSLogger = () => {
     };
     const logger = Logger(loggerType);
     console.log(`Logging screen information for path: ${serviceName}`);
-    logger.event.name = name;
-
-    setLocalStorage(logger);
+    logger.event.path = path;
+    if (serviceName) {
+      logger.event.serviceName = serviceName;
+    }
+    return logger;
   };
 
   const click = ({ name }: LogPayloadParams) => {
     console.log(`Logging click information for button: ${name}`);
-    //사용자에서 path,name,message를 넣어줌
     const loggerType: LoggerType = {
       path: '/',
       serviceName: 'home',
@@ -49,12 +38,9 @@ export const useYLSLogger = () => {
       message: '/',
     };
     const logger = Logger(loggerType);
-
     logger.event.name = name;
-
-    setLocalStorage(logger);
+    return logger;
   };
-  // todo: 로컬스토리지 로그 개수가 10개 넘어가면 postLog.ts호출
 
   return {
     screen,
--- src/demo/Drawer.tsx
+++ src/demo/Drawer.tsx
@@ -2,28 +2,25 @@ import { useState } from 'react';
 import { LogClick } from '../LogClick';
 import { useLocation } from 'react-router-dom';
 import { LogScreen } from '../LogScreen';
-
-export const Home = () => {
+export const Drawer = () => {
   const [count, setCount] = useState(0);
   const router = useLocation();
-
+  console.log(router.pathname);
   return (
     <>
-      <h1>Home</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <LogScreen
           params={{
             userId: 123,
             path: router.pathname,
             name: '',
-            serviceName: 'drawer',
           }}
         >
           <LogClick
             params={{
               userId: 123,
               name: 'click',
-              serviceName: 'drawer',
               path: router.pathname,
             }}
           >
