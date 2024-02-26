aa75b5ef65b744aaf1364a9a5f76d9c5efdf0dbd
Hanna922 <hanna.urssu@gmail.com>
2024-02-26 23:08:54 +0900

    feat: modify Type
    - 모든 Type 수정

--- src/types/LogType.ts
+++ src/types/LogType.ts
@@ -1,25 +1,23 @@
-export type ServiceNameType = 'drawer' | 'home' | 'search';
-
-// LogType: 최종 Log 형태
-export interface LogType {
-  hashedId: string;
-  timestamp: string;
-  event: {
-    platform: string;
-    serviceName: ServiceNameType;
-    name: string;
-    message?: string;
-    path?: string;
-    tags?: string[];
-  };
-}
-
 // LogPayloadParams: 사용처에서 넣어주는 값
 export interface LogPayloadParams {
   userId: string;
-  name: string | '';
-  serviceName: ServiceNameType;
-  message?: string;
-  path?: string;
-  tags?: string[];
+  version: number;
+  event: any[];
+}
+
+// LogType: 최종 Log 형태
+export interface LogType {
+  hashedID: string;
+  timestamp: string;
+  // version: number;
+  event: LogPayloadParams['event'];
+}
+
+export interface LogRequestList {
+  logRequestList: LogType[];
+}
+
+export interface LogResponse {
+  success: boolean;
+  result: { [key: string]: any };
 }
